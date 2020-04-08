686ec00062a4217046b09b06405b988e615c25d9
vick <vicknogueira199@gmail.com>
2020-04-08 17:06:50 -0300

    Implementação das rotas home e feed

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ // { } -> é objeto
+  { path: '', redirectTo: 'home', pathMatch: 'full'}, // Para quando a rota estiver vazia, ele redirecionar para home
+  { path: 'home', component: HomeComponent}, //Criamos uma rota home, que vai trazer o componente HomeComponent
+  { path: 'feed', component: FeedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
