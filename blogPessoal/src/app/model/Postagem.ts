a73cf53d59ece641e35da1b0cb427c2615259a94
Vitória <vicknogueira199@gmail.com>
2020-04-15 12:26:09 -0300

    Criação da Model e do Service :)

---
+++ blogPessoal/src/app/model/Postagem.ts
@@ -0,0 +1,7 @@
+export class Postagem{ // É assim que se cria uma classe
+
+   public id:number 
+   public titulo:string
+   public texto:string
+   public data:Date
+}
---
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService { //Será nessa classe que trabalharemos iremos construir aqui todo o nosso contato com o back-end
+
+  constructor() { }
+}
